7f91018c351feeae7ec344b90b32578ad197a836
PableKe <pableke@users.noreply.github.com>
2017-02-25 19:59:39 +0100

    jQuery page-list version
    Trsnslate page-list repository to a jQuery version

--- page-list.js
+++ page-list.js
@@ -1,79 +1,83 @@
 
-//funciones para la gestion del paginado
-function pagelist(elem, opts) {
-	opts = opts || {};
-	opts.items = opts.items || 0;
-	opts.itemsOnPage = opts.itemsOnPage || 1;
-	opts.currentPage =  opts.currentPage || 1;
-	opts.displayedPages = opts.displayedPages || 3;
-	opts.className = opts.className || "page-list";
-	opts.rangeText = opts.rangeText || "...";
-	opts.prevText = opts.prevText || "<";
-	opts.nextText = opts.nextText || ">";
-
-	var _range = function(val, min, max) {
-		return Math.min(Math.max(val, min), max);
+(function($) {
+	var self, pages;
+	var settings = {
+		items: 0,
+		itemsOnPage: 1,
+		currentPage: 1,
+		displayedPages: 3,
+		className: "page-list",
+		rangeText: "&hellip;",
+		prevText: "&lt;",
+		nextText: "&gt;"
 	};
 
-	var _click = function(pages, page) {
-		opts.currentPage = _range(page, 1, pages);
-		opts.onClick && opts.onClick(opts.currentPage);
-		_self.draw(opts.currentPage);
+	function range(val, min, max) { return Math.min(Math.max(val, min), max); };
+	function rangePages(val) { return range(val, 1, pages); };
+	function setPages() { pages = actions.numPages(); return actions; };
+	function draw(elem, page) {
+		//inicialize page and sub-treee DOM
+		while (elem.firstChild) elem.removeChild(elem.firstChild);
+		var ul = elem.appendChild(document.createElement("ul"));
+
+		//calc the iterator index and range limits
+		settings.currentPage = rangePages(page || settings.currentPage); //page 0 not allowed
+		var end = range(settings.currentPage + settings.displayedPages, 0, pages); //last page
+		var start = range(settings.currentPage - settings.displayedPages - 1, 0, end);
+		var group = (settings.displayedPages * 2) + 1;
+		start = (end == pages) ? Math.max(end - group, 0) : start;
+		end = (start == 0) ? Math.min(group, pages) : end;
+
+		//add prev button and first page (if is necesary)
+		append(ul, settings.currentPage - 1, settings.prevText);
+		(start > 0) && append(ul, 1);
+		(start > 1) && append(ul, settings.currentPage - group, settings.rangeText);
+
+		//iterate over displayed pages
+		var i = start * settings.itemsOnPage;
+		while ((i < settings.items) && (start < end)) {
+			append(ul, ++start);
+			i += settings.itemsOnPage;
+		}
+
+		//add next button and last page (if is necesary)
+		(end < (pages - 1)) && append(ul, settings.currentPage + group, settings.rangeText);
+		(end < pages) && append(ul, pages);
+		append(ul, settings.currentPage + 1, settings.nextText);
+
+		ul.setAttribute("class", settings.className);
+		$("li[name='" + settings.currentPage + "']", ul).text(settings.currentPage);
 	};
 
-	var _append = function(elem, pages, page, text) {
+	function clickOn(page) {
+		actions.draw(page); //update list page
+		settings.onClick && settings.onClick(settings.currentPage);
+	};
+
+	function append(elem, page, text) {
 		var li = $('<li name="' + page + '"></li>');
 		var a = $('<a href="#page-' + page + '">' + (text || page) + '</a>');
-		$(elem).append(li.append(a.click(function() { _click(pages, page); })));
+		$(elem).append(li.append(a.click(function() { clickOn(page); })));
 	};
 
-	var _self = {
-		numItems: function() { return opts.items; },
-		setItems: function(i) { opts.items = i; return this; },
-		currentPage: function() { return opts.currentPage; },
-		setCurrentPage: function(p) { opts.currentPage = p; return this; },
-		itemsOnPage: function() { return opts.itemsOnPage; },
-		setItemsOnPage: function(l) { opts.itemsOnPage = l; return this; },
-		numPages: function() { return Math.ceil(opts.items / opts.itemsOnPage); },
-		firstItemOnPage: function(page) { return page ? ((page - 1) * opts.itemsOnPage) : 0; },
-		getPageFromIndex: function(i) { return i ? Math.ceil(i / opts.itemsOnPage) : 1; },
-
-		draw: function(page) {
-			//inicialize page and sub-treee DOM
-			opts.currentPage = page || opts.currentPage;
-			while (elem.firstChild) elem.removeChild(elem.firstChild);
-			var ul = elem.appendChild(document.createElement("ul"));
-
-			//calc the iterator index and range limits
-			var pages = this.numPages();
-			opts.currentPage = _range(opts.currentPage, 1, pages);
-			var end = _range(opts.currentPage + opts.displayedPages, 0, pages);
-			var start = _range(opts.currentPage - opts.displayedPages - 1, 0, end);
-			var range = (opts.displayedPages * 2) + 1;
-			start = (end == pages) ? Math.max(end - range, 0) : start;
-			end = (start == 0) ? Math.min(range, pages) : end;
-
-			//add prev button and first page (if is necesary)
-			_append(ul, pages, opts.currentPage - 1, opts.prevText);
-			(start > 0) && _append(ul, pages, 1);
-			(start > 1) && _append(ul, pages, opts.currentPage - range, opts.rangeText);
-
-			//iterate over displayed pages
-			var i = start * opts.itemsOnPage;
-			while ((i < opts.items) && (start < end)) {
-				_append(ul, pages, ++start);
-				i += opts.itemsOnPage;
-			}
-
-			//add next button and last page (if is necesary)
-			(end < (pages - 1)) && _append(ul, pages, opts.currentPage + range, opts.rangeText);
-			(end < pages) && _append(ul, pages, pages);
-			_append(ul, pages, opts.currentPage + 1, opts.nextText);
-
-			ul.setAttribute("class", opts.className);
-			$("li[name='" + opts.currentPage + "']", ul).html(opts.currentPage);
-			return this;
-		}
+	var actions = {
+		currentPage: function() { return settings.currentPage; },
+		setCurrentPage: function(p) { settings.currentPage = p; return this; },
+		numItems: function() { return settings.items; },
+		setItems: function(i) { settings.items = i; return setPages(); },
+		itemsOnPage: function() { return settings.itemsOnPage; },
+		setItemsOnPage: function(l) { settings.itemsOnPage = l; return setPages(); },
+		firstItemOnPage: function(page) { return page ? ((page - 1) * settings.itemsOnPage) : 0; },
+		getPageFromIndex: function(i) { return i ? Math.ceil(i / settings.itemsOnPage) : 1; },
+		numPages: function() { return Math.ceil(settings.items / settings.itemsOnPage); },
+		draw: function(page) { self.each(function() { draw(this, page); }); return this; },
+		end: function() { return self; }
 	};
-	return _self;
-};
+
+	$.fn.pagelist = function(opts, value) {
+		self = this; //global reference to the jquery list
+		opts && $.extend(settings, opts); //inicialize settings
+		return setPages(); //calc the total page number
+	};
+
+}(jQuery));

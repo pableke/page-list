d2029efce5f90a8789eb9c6f694b594c30fee0cf
PableKe <pableke@users.noreply.github.com>
2017-03-09 13:08:12 +0100

    Manage multiple page-list in the same page
    This commit allow users to manage multiple page lists elements into the same web page

--- page-list.js
+++ page-list.js
@@ -1,6 +1,4 @@
-
 (function($) {
-	var self; //all page-lists reference
 	var settings = { //global config
 		items: 0,
 		itemsOnPage: 1,
@@ -13,71 +11,67 @@
 	};
 
 	function range(val, min, max) { return Math.min(Math.max(val, min), max); };
-	function draw(elem, page) {
+	function drawPageList(elem, pages, page, actions, settings) {
 		//inicialize page and sub-treee DOM
 		while (elem.firstChild) elem.removeChild(elem.firstChild);
 		var ul = elem.appendChild(document.createElement("ul"));
 
 		//calc the iterator index and range limits
-		var pages = actions.numPages(); //total number of pages
 		page = page || settings.currentPage; //set default page number
-		settings.currentPage = range(page, 1, pages); //page 0 not allowed
-		var end = range(settings.currentPage + settings.displayedPages, 0, pages); //last page
-		var start = range(settings.currentPage - settings.displayedPages - 1, 0, end);
+		page = settings.currentPage = range(page, 1, pages); //page starts in 1
+		var end = range(page + settings.displayedPages, 0, pages); //last page
+		var start = range(page - settings.displayedPages - 1, 0, end);
 		var group = (settings.displayedPages * 2) + 1;
 		start = (end == pages) ? Math.max(end - group, 0) : start;
 		end = (start == 0) ? Math.min(group, pages) : end;
 
 		//add prev button and first page (if is necesary)
-		append(ul, settings.currentPage - 1, settings.prevText);
-		(start > 0) && append(ul, 1);
-		(start > 1) && append(ul, settings.currentPage - group, settings.rangeText);
+		append(ul, page - 1, actions, settings.prevText);
+		(start > 0) && append(ul, 1, actions);
+		(start > 1) && append(ul, page - group, actions, settings.rangeText);
 
 		//iterate over displayed pages
 		var i = start * settings.itemsOnPage;
 		while ((i < settings.items) && (start < end)) {
-			append(ul, ++start);
+			append(ul, ++start, actions);
 			i += settings.itemsOnPage;
 		}
 
 		//add next button and last page (if is necesary)
-		(end < (pages - 1)) && append(ul, settings.currentPage + group, settings.rangeText);
-		(end < pages) && append(ul, pages);
-		append(ul, settings.currentPage + 1, settings.nextText);
+		(end < (pages - 1)) && append(ul, page + group, actions, settings.rangeText);
+		(end < pages) && append(ul, pages, actions);
+		append(ul, page + 1, actions, settings.nextText);
 
 		ul.setAttribute("class", settings.className);
-		$("li[name='" + settings.currentPage + "']", ul).text(settings.currentPage);
+		$("li#p" + page, ul).text(page);
 	};
 
-	function clickOn(page) {
-		actions.draw(page); //update list page
-		settings.onClick && settings.onClick(settings.currentPage);
-	};
-
-	function append(elem, page, text) {
-		var li = $('<li name="' + page + '"></li>');
+	function append(elem, page, actions, text) {
+		var li = elem.appendChild(document.createElement("li"));
 		var a = $('<a href="#page-' + page + '">' + (text || page) + '</a>');
-		$(elem).append(li.append(a.click(function() { clickOn(page); })));
+		$(li).attr("id", "p" + page).append(a.click(function() { actions.click(page); }));
 	};
 
-	var actions = {
-		currentPage: function() { return settings.currentPage; },
-		setCurrentPage: function(p) { settings.currentPage = p; return this; },
-		numItems: function() { return settings.items; },
-		setItems: function(i) { settings.items = i; return this; },
-		itemsOnPage: function() { return settings.itemsOnPage; },
-		setItemsOnPage: function(l) { settings.itemsOnPage = l; return this; },
-		firstItemOnPage: function(page) { return page ? ((page - 1) * settings.itemsOnPage) : 0; },
-		getPageFromIndex: function(i) { return i ? Math.ceil(i / settings.itemsOnPage) : 1; },
-		numPages: function() { return Math.ceil(settings.items / settings.itemsOnPage); },
-		draw: function(page) { self.each(function() { draw(this, page); }); return this; },
-		click: function(page) { clickOn(page); return this; },
-		end: function() { return self; }
-	};
-
-	$.fn.pagelist = function(opts, value) {
-		self = this; //global reference to the jquery list
-		opts && $.extend(settings, opts); //inicialize settings
-		return actions; //return actions page-list object
+	$.fn.pagelist = function(opts) {
+		var self = this; //global reference to jquery list
+		opts = $.extend({}, settings, opts || {});
+		return { //return actions page-list object
+			currentPage: function() { return opts.currentPage; },
+			setCurrentPage: function(p) { opts.currentPage = p; return this; },
+			numItems: function() { return opts.items; },
+			setItems: function(i) { opts.items = i; return this; },
+			itemsOnPage: function() { return opts.itemsOnPage; },
+			setItemsOnPage: function(l) { opts.itemsOnPage = l; return this; },
+			firstItemOnPage: function(page) { return page ? ((page - 1) * opts.itemsOnPage) : 0; },
+			getPageFromIndex: function(i) { return i ? Math.ceil(i / opts.itemsOnPage) : 1; },
+			numPages: function() { return Math.ceil(opts.items / opts.itemsOnPage); },
+			draw: function(page) {
+				var pages = this.numPages();
+				self.each(function(i, e) { drawPageList(e, pages, page, this, opts); });
+				return this;
+			},
+			click: function(page) { opts.onClick && opts.onClick(page); return this.draw(page); },
+			end: function() { return self; }
+		};
 	};
 }(jQuery));
